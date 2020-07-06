67730fb115b9a40d9db73d460bed795cc8cdb3e4
unknown <nadavpopo@gmail.com>
2020-07-06 17:43:03 +0300

    06 07 2020

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import AppMusic from './comps_music/appMusic';
 
-function App()
-{
+function App() {
   return (
     <div className="App">
-      <div className="container">
-       <AppMusic/>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
