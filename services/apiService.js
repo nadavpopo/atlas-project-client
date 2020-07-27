0abf81f76d879c0ccc27293804f994f6a7cbdd34
unknown <nadavpopo@gmail.com>
2020-07-27 16:00:20 +0300

    27/07/2020

--- services/apiService.js
+++ services/apiService.js
@@ -1,3 +1,9 @@
+let localUrl = "http://localhost:3000/";
+let realUrl = "https://marketcor.herokuapp.com/"
+export const apiUrl = localUrl;
+// כמה מוצרים להציג בכל עמוד מקסימום
+export let perPage = 4;
+
 export const doApiGet = async (_url) => {
     let resp = await fetch(_url);
     let data = await resp.json();
@@ -19,3 +25,16 @@ export const doApiPost = async (_url, _body) => {
 }
 
 
+export const doApiPostToken = async (_url, _body) => {
+    let resp = await fetch(_url, {
+        method: "POST",
+        body: JSON.stringify(_body),
+        headers: {
+            'content-type': "application/json",
+            'x-auth-token':localStorage["tok"]
+        }
+    })
+    let data = await resp.json()
+    console.log("service say:" ,data)
+    return data;
+}
